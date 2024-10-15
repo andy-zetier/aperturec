e450f47a29f8217b7f140acf6e8040ffb2901380
joe <joe@zetier.com>
2024-10-15 18:35:02 +0000

    Add RPM packaging

--- ci/scripts/package_upload.sh
+++ ci/scripts/package_upload.sh
@@ -10,10 +10,10 @@ if [[ ! -d "${REPO}" ]]; then REPO="$PWD/../"; fi
 # Define the base directory
 BASE_DIR="$REPO/target"
 
-# Initialize a counter for .deb files
-deb_counter=0
+# Initialize a counter for .deb/.rpm files
+counter=0
 
-# Loop through each distribution (e.g., debian, ubuntu)
+# Loop through each distribution (e.g., debian, ubuntu, fedora)
 for distro in "$BASE_DIR"/*; do
     if [ -d "$distro" ]; then
         distro_name=$(basename "$distro")
@@ -23,14 +23,14 @@ for distro in "$BASE_DIR"/*; do
             if [ -d "$release" ]; then
                 release_name=$(basename "$release")
 
-                # Loop through each .deb file
-                for deb_package in "$release"/*.deb; do
-                    if [ -f "$deb_package" ]; then
-                        deb_counter=$((deb_counter + 1))
-                        echo "Uploading $deb_package to $distro_name/$release_name ..."
+                # Loop through each .deb/.rpm file
+                for package in "$release"/*.deb "$release"/*.rpm; do
+                    if [ -f "$package" ]; then
+                        counter=$((counter + 1))
+                        echo "Uploading $package to $distro_name/$release_name ..."
 
                         # Upload the package
-                        package_cloud push zetier/aperturec/$distro_name/$release_name "$deb_package"
+                        package_cloud push zetier/aperturec/"$distro_name"/"$release_name" "$package"
                     fi
                 done
             fi
@@ -38,8 +38,8 @@ for distro in "$BASE_DIR"/*; do
     fi
 done
 
-# Check if no .deb files were found
-if [ $deb_counter -eq 0 ]; then
-    echo "Error: No .deb files found to upload."
+# Check if no .deb/.rpm files were found
+if [ $counter -eq 0 ]; then
+    echo "Error: No .deb or .rpm files found to upload."
     exit 1
 fi
