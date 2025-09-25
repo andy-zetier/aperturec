bf2ec77439410f232b59baab91e37b39f28c30ea
andy-zetier <andy.myers@zetier.com>
2025-09-25 18:43:12 -0400

    ci: Add MSI signing (#578)
    Adds signing for the Windows MSI installer. Signing is via Microsoft's
    Trusted Signing service through Azure. Requires a Trusted Signing
    Account and Certificate profile setup through Azure along with a
    Registered Application in Entra ID to handle access to the signing
    material.
    
    Jsign (https://ebourg.github.io/jsign/) is used for the actual signing
    instead of the Windows-only Microsoft SignTool.exe.
    
    The following Azure identity values are required and currently setup as
    GitHub actions secrets:
    
    * AZURE_TENANT_ID  - Azure Tenant ID (Zetier Inc)
    * AZURE_CERT_ALIAS - <Trusted Signing Account>/<Certificate profile>
    * AZURE_APP_ID     - Registered Application ID (aka Client ID) (Jsign)
    * AZURE_APP_SECRET - Registered Application ID secret (Jsign)

--- ci/scripts/create-windows-package.sh
+++ ci/scripts/create-windows-package.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-# ==== derive product version from root Cargo.toml ====
+# derive product version from root Cargo.toml
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 ROOT_DIR=$(dirname "$(dirname "$SCRIPT_DIR")")
 SRC_DIR="${ROOT_DIR}/aperturec-client-gtk3"
@@ -10,7 +10,6 @@ version=$(
   grep -m1 '^version *= *"' "$SRC_DIR/Cargo.toml" \
     | sed -E 's/.*version *= *"([^"]+)".*/\1/'
 )
-
 
 mkdir -p package
 cp aperturec-client.exe package
@@ -41,11 +40,43 @@ find package -maxdepth 1 -type f -exec mingw-strip {} +
 
   # compile MSI
   wixl -v -a x64 \
-	  -D SourceDir="$(pwd)/" \
-	  -D Win64=yes \
-	  -D Version="${version}" \
-	  -o "../aperturec-client_${version}".msi \
-	  "${ROOT_DIR}/ci/package.wxs" files.wxs
+    -D SourceDir="$(pwd)/" \
+    -D Win64=yes \
+    -D Version="${version}" \
+    -o "../aperturec-client_${version}".msi \
+    "${ROOT_DIR}/ci/package.wxs" files.wxs
+
+  # pull jsign
+  JSIGN_URI="https://github.com/ebourg/jsign/releases/download/7.1/jsign-7.1.jar"
+  curl -O ${JSIGN_URI} -L
+
+  # The following environment variables are GitHub secrets:
+  #   AZURE_APP_ID
+  #   AZURE_APP_SECRET
+  #   AZURE_TENNANT_ID
+  #   AZURE_CERT_ALIAS
+
+  # sign into azure and fetch temporary access token
+  az login \
+    --service-principal \
+    --username "${AZURE_APP_ID}" \
+    --password "${AZURE_APP_SECRET}" \
+    --tenant "${AZURE_TENANT_ID}"
+
+  AZ_TOKEN=$(
+    az account get-access-token --resource https://codesigning.azure.net |
+    jq -r .accessToken \
+  )
+
+  # sign MSI
+  java -jar "${JSIGN_URI##*/}" \
+    --storetype TRUSTEDSIGNING \
+    --keystore eus.codesigning.azure.net \
+    --storepass "${AZ_TOKEN}" \
+    --alias "${AZURE_CERT_ALIAS}" \
+    --name "ApertureC Client" \
+    --url "www.zetier.com" \
+    "../aperturec-client_${version}".msi
 )
 
 rm -rf package
